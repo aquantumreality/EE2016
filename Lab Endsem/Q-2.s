fd7bd17b62142dcacdc557031934b73d01d1aea8
Abhiram Rao <73462883+aquantumreality@users.noreply.github.com>
2021-11-23 06:07:23 +0530

    Update Q-2.s

--- Lab Endsem/Q-2.s
+++ Lab Endsem/Q-2.s
@@ -1,47 +1,26 @@
-1             START:
+START:
 
-2                           LDI         ZH, 0x08
+LDI      ZH, 0x08
+LDI      ZL, 0x00                           
+LPM      R2, Z                                               
+LDI      R20, 0xF0
+MOV      R3, R20
+AND      R3, R2                                     
+LDI      R21, 0x0F
+AND      R2, R21                  
+LDI      R22, 0x30
+MOV      R4, R22                           
+CP       R3, R4  
+BREQ    DISP
+LDI      R23, 0x0A
+MOV      R2, R23                             
 
-3                           LDI         ZL, 0x00                           
 
-4
-
-5                           LPM       R2, Z                                               
-
-6                            LDI         R20, 0xF0
-
-7                            MOV      R3, R20
-
-8                            AND       R3, R2                                     
-
-9                            LDI         R21, 0x0F
-
-10                         AND       R2, R21                  
-
-11                         LDI         R22, 0x30
-
-12                         MOV      R4, R22                           
-
-13                         CP          R3, R4  
-
-14                         BREQ    DISP
-
- 15                        LDI         R23, 0x0A
-
- 16                        MOV      R2, R23                             
-
- 17    DISP:          LDI         ZH, 0x20
-
- 18                        LDI         ZL, 0x00
-
- 19                        MOV      ZL, R2
-
- 20                        LPM       R5, Z
-
- 21                        LDI         XH, 0x40
-
- 22                        LDI         XL, 0x30     
-
- 23                        ST          X, R5                  
-
- 24                        RJMP    START
+DISP:    LDI ZH, 0x20
+LDI      ZL, 0x00
+MOV      ZL, R2
+LPM      R5, Z
+LDI      XH, 0x40
+LDI      XL, 0x30     
+ST       X, R5                  
+RJMP    START
