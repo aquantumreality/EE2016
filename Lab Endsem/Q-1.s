61fa8b4d9ffaa900bbaa550fc21523235aabe502
Abhiram Rao <73462883+aquantumreality@users.noreply.github.com>
2021-11-23 06:03:53 +0530

    Update Q-1.s

--- Lab Endsem/Q-1.s
+++ Lab Endsem/Q-1.s
@@ -1,63 +1,37 @@
-7 ENTRY
+ENTRY
 
-  8
+Main
 
-  9 Main
+LDR R0, Number1
+LDR R1, Number2
+MOV R3, #0
 
-10                               LDR R0, Number1
+Loop
+CMP R1, #0
+BEQ Err
+CMP R0, R1
+BLT Done
+ADD R3, R3, #1
+SUB R0, R0, R1
+B Loop
 
-11                               LDR R1, Number2
+Err
+MOV R3, #0xFFFFFFFF
 
-12                               MOV R3, #0
+Done
+STR R0, Remain
+STR R3, Quotient
 
-13 Loop
+SWI &11
 
-14                               CMP R1, #0
+ AREA Data1, DATA
+ Number1            DCD &0276CBC1
+ Number2            DCD &0286
+  ALIGN
+                             
+ AREA Data2, DATA
+Quotient             DCD 0
+Remain                DCD 0
+  ALIGN
 
-15                               BEQ Err
-
-16                               CMP R0, R1
-
-17                               BLT Done
-
-18                               ADD R3, R3, #1
-
-19                               SUB R0, R0, R1
-
-20                               B Loop
-
-21 Err
-
-22                               MOV R3, #0xFFFFFFFF
-
-23 Done
-
-24                               STR R0, Remain
-
-25                               STR R3, Quotient
-
-26                               SWI &11
-
-27
-
-28                               AREA Data1, DATA
-
-29 Number1            DCD &0276CBC1
-
-30 Number2            DCD &0286
-
-31                               ALIGN
-
-32
-
-33                               AREA Data2, DATA
-
-34 Quotient             DCD 0
-
-35 Remain                DCD 0
-
-36                               ALIGN
-
-37
-
-38                               END
+END
